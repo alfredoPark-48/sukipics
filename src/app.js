114143c2ca3c18a12085443de46d2d18e9290f08
alfredoPark-48 <parkalfredojeonghyun@gmail.com>
2021-04-28 19:02:44 -0500

    Express server added

--- src/app.js
+++ src/app.js
@@ -1,20 +1,41 @@
 const Danbooru = require("danbooru");
+const express = require("express");
+const path = require("path");
 
 const booru = new Danbooru();
+const publicPath = path.join(__dirname + "/../public/");
+
+console.log(publicPath);
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// For CSS Styles
+app.use(express.static(publicPath));
+
+app.get("/", (req, res) => {
+	res.sendFile(publicPath + "index.html");
+});
 
 booru.posts({ tags: "rating:safe order:rank" }).then((posts) => {
 	// Select a random post from posts array
 	const index = Math.floor(Math.random() * posts.length);
 	const post = posts[index];
 
-	console.log(post);
+	// console.log(post);
 
 	// Get post's url and create a filename for it
 	const url = booru.url(post.file_url);
 	const name = `${post.md5}.${post.file_ext}`;
 
+	// console.log(url.href);
+
 	// Download post image using node's https and fs libraries
-	require("https").get(url, (response) => {
-		response.pipe(require("fs").createWriteStream(name));
-	});
+	// require("https").get(url, (response) => {
+	// 	response.pipe(require("fs").createWriteStream(name));
+	// });
+});
+
+app.listen(port, () => {
+	console.log(`Listening at port ${port}`);
 });
